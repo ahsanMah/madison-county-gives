ab83e04ff07b3820192409120dc0333078257d49
ashin1995 <ashin@colgate.edu>
2017-11-04 17:11:14 -0400

    more tests for TouchNet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101202147) do
+ActiveRecord::Schema.define(version: 20171102005026) do
 
   create_table "campaigns", force: :cascade do |t|
     t.integer "organization_id"
@@ -26,6 +26,20 @@ ActiveRecord::Schema.define(version: 20171101202147) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.string "name"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.integer "campaign_id"
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.decimal "amount", precision: 10, scale: 2
+    t.integer "transaction_id"
+    t.datetime "time"
+    t.boolean "is_anonymous"
+    t.boolean "is_konosioni"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
