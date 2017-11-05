9c4714f079066e9820c9b35ec4aebfa9d5ef6dcf
ryumaggs <ryu@colgate.edu>
2017-11-04 21:11:59 -0400

    added checkout page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,7 @@ class HomeController < ApplicationController
 
   def index
   end
+  def cart
+    @carty = session[:cart]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :campaign, only: [:index, :show]
   root "home#index"
   resource :campaigns
+  get '/cart', to: 'home#cart'
 end
