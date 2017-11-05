52a7d4f3b0d67023566d8a11cab549db45d188ea
J4KLin <qianniao888@yahoo.com>
2017-11-04 22:06:35 -0400

    Wrote some step definitions for campaign create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :organization, except: :destroy
-  resources :campaign, only: [:index, :show]
+  resources :organizations, except: :destroy
+  #resources :campaigns, only: [:index, :show]
   root "home#index"
   resource :campaigns
 end
