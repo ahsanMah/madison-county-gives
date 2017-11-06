037990cea124b67692fc3dcb545a7c18987019c7
smaug <mahmood.ahsan@outlook.com>
2017-11-06 17:01:06 -0500

    Feature passed: Donor viewing specific campaign

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -144,7 +144,7 @@ When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
   attach_file(field, File.expand_path(path))
 end
 
-Then /^(?:|I )should see '([^']*)'$/ do |text|
+Then /^(?:|I )should see ['|"]([^']*)['|"]$/ do |text|
   if page.respond_to? :should
     page.should have_content(text)
   else
