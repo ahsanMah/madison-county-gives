2c44570b8ec2c32d2c53b7d6ca0ab0a561da9a8e
smaug <mahmood.ahsan@outlook.com>
2017-11-07 18:38:41 -0500

    * Added campaign_changes controller method: approve * Campaign changes now have associated campaign ids and actions * Updated routes for approved method

--- app/controllers/campaign_changes_controller.rb
+++ app/controllers/campaign_changes_controller.rb
@@ -1,31 +1,37 @@
 class CampaignChangesController < ApplicationController
 
   def show
-    @campaigns = CampaignChange.find(params[:id])
+    @campaign = CampaignChange.find(params[:id])
   end
 
   def new
-    @campaign = CampaignChange.new
-    if !params[:campaign_id].nil?
-      Campaign.find(params[:campaign_id]).attributes.each do |curKey, curVal|
-        if @campaign.has_attribute? curKey
-          @campaign[curKey] = curVal
+    campaign_change = CampaignChange.new
+
+    #Populate change with existing campaign fields
+    if params[:campaign_id]
+      exiting_campaign = Campaign.find(params[:campaign_id])
+      existing_campaign.attributes.each do |curKey, curVal|
+        if campaign_change.has_attribute? curKey
+          campaign_change[curKey] = curVal
         end
       end
+      campaign_change.campaign_id = existing_campaign.id
     end
+
+    campaign_change.action = params[:campaign_action] || "CREATE"
+    campaign_change.organization_id = current_user.organization.id
+    
+    @campaign = campaign_change
   end
 
   def create
     campaign = CampaignChange.new(create_update_params)
-    campaign.action = "CREATE"
-    if !campaign.id.nil?
-      campaign.action = "UPDATE"
-    end
+
     if campaign.save
 			flash[:notice] = "Campaign proposal for \"#{campaign.name}\" successfully submitted for approval!"
-			redirect_to campaigns_path and return
+			redirect_to organization_path current_user.organization and return
 		else
-			flash[:error] = "Unable to submit!"
+			flash[:error] = "Unable to submit campaign proposal!"
 			redirect_to new_campaign_change_path
 		end
   end
@@ -41,10 +47,37 @@ class CampaignChangesController < ApplicationController
     if campaign.save
       flash[:notice] = "Updates for \"#{campaign.name}\" successfully submitted for approval!"
       redirect_to campaigns_path and return
-    else
-      flash[:error] = "Unable to submit!"
-      redirect_to edit_campaign_change_path
     end
+    
+    #Unable to save
+    flash[:error] = "Unable to submit \"#{campaign.name}\" for approval!"
+    
+    if campaign.action == "CREATE"
+      redirect_to new_campaign_change_path @campaign and return
+    end
+
+    redirect_to edit_campaign_change_path @campaign
+  end
+
+  def approve
+    @pending_campaign = CampaignChange.find(params[:id])
+    @approved_campaign = Campaign.new()
+    
+    @pending_campaign.attributes.each do |curKey, curVal|
+        if @approved_campaign.has_attribute? curKey
+          @approved_campaign[curKey] = curVal
+        end
+    end
+
+    if @approved_campaign.save
+      flash[:notice] = "Campaign has been successfully approved!"
+      @pending_campaign.destroy
+      redirect_to campaign_path @approved_campaign and return
+    else
+      flash[:error] = "Oops! We failed to approve this campaign, please try again."
+      redirect_to campaign_change_path @pending_campaign
+    end
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :organizations, except: :destroy
   resources :campaigns #, only: [:index, :show, :new, :create]
   resources :campaign_changes
+  get 'campaign_changes/approve/:id', to: "campaigns_change#approve"
+  
   root "campaigns#index"
   get '/summary', to: 'home#summary'
   get '/checkout', to: 'home#checkout'
