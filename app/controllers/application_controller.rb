009d4c5c9200695d6cffd0defd7d1057203a392b
Boris Shou <borisshou@gmail.com>
2017-11-06 22:15:32 -0500

    Created login/out links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+    def after_sign_in_path_for(resource)
+      if current_user.organization
+        organization_path current_user.organization.id
+      else
+        root_path
+      end
+    end
 end
