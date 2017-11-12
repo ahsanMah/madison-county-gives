b2718af2a427db9ad9dc6abbe357f6151910b874
smaug <mahmood.ahsan@outlook.com>
2017-11-12 12:48:05 -0500

    Added Delete functionality and fixed redirection bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :organizations, except: :destroy
-  resources :campaigns #, only: [:index, :show, :new, :create]
+  resources :campaigns , only: [:index, :show]
   resources :campaign_changes
   get 'campaign_changes/approve/:id', to: "campaign_changes#approve", as: :approve_campaign_change
+  get 'campaign_changes/delete/:id', to: "campaign_changes#delete", as: :delete_campaign_change
 
   root "campaigns#index"
   get '/summary', to: 'home#summary'
