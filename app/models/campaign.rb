034b3bc2b0298b01873f63dd0b4fa2dcb66b0e90
ashin1995 <ashin@colgate.edu>
2017-11-16 18:52:40 -0500

    payment associations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,6 @@
 class Campaign < ApplicationRecord
   has_one :campaign_change
+  has_many :payments
   belongs_to :organization
   has_attached_file :image, :styles=> {:large => "1000x700>", :thumb => "500x350>"},
   :default_url => "default.png"
