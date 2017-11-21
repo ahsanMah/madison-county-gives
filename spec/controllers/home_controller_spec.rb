ca0b1263ca45e5bb87605f37f70aff2bf15de333
csvm <qianniao888@yahoo.com>
2017-11-21 13:37:50 -0500

    some rspec test for home

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -9,4 +9,27 @@ RSpec.describe HomeController, type: :controller do
 			# expect {post :create_payment, payment_params}.to change(Payment, :count).by(1)
 		end
 	end
+
+	describe "GET #summary" do
+		it "renders the summary template" do
+			c1 = Campaign.new(:id => 1)
+				allow(Campaign).to receive(:find).with(1) { c1 }
+			c2 = Campaign.new(:id => 2)
+				allow(Campaign).to receive(:find).with(2) { c2 }
+			get :summary, :session => {:cart => [[1,1], [2,1]]}
+			expect(response).to render_template(:summary)
+		end
+	end
+
+	describe "GET #checkout" do
+		it "renders the checkout template" do
+			c1 = Campaign.new(:id => 1)
+				allow(Campaign).to receive(:find).with(1) { c1 }
+			c2 = Campaign.new(:id => 2)
+				allow(Campaign).to receive(:find).with(2) { c2 }
+			get :checkout, :session => {:cart => [[1,1], [2,1]]}
+			expect(response).to render_template(:checkout)
+		end
+	end
+
 end
