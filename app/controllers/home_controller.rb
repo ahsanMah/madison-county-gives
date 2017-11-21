689c548a2fe27cda1a98b05be10fff3f73d8c21c
ryumaggs <ryu@colgate.edu>
2017-11-20 19:20:14 -0500

    functional cart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,19 +4,32 @@ class HomeController < ApplicationController
   def index
   end
   def summary
-    @carty = session[:cart]
+    carty = session[:cart]
+    @names_val = {}
+    carty.each do |id, val|
+      @names_val[Campaign.find(id).name] = val
+    end
   end
   def checkout
     @carty = session[:cart]
     @US_states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    @total = 0
+    @carty.each do |id, amt|
+      @total = @total + amt.to_i
+    end
   end
   def processing
      amt = params[:amount_to_cart]
      id = params[:id_to_cart]
      puts "#{amt} ++++++++++++++++ #{id}"
      if amt != nil
-       puts "GOT TO SESSION ADDING"
-       session[:cart] = {id => amt}
+       if session[:cart][id]
+         session[:cart][id] = (session[:cart][id].to_i + amt.to_i).to_s
+       else
+         session[:cart][id] = amt
+     end
+     elsif params[:pmt_amt]
+       puts "touchnet posted back"
      end
      redirect_to root_path
      return
