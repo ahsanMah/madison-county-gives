9ad8ef6c4875c8d8573dad58a403466400e80cd7
Boris Shou <borisshou@gmail.com>
2017-12-02 00:05:30 -0500

    Added more to seed, added ded explanation to image upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,18 +17,29 @@ def seed_image(file_name)
   File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}"))
 end
 
-User.create!(id: 1, email: "bg8s@colgate.edu", password: "123456")
-User.create!(id: 2, email: "pastapls@yahoo.com", password: "123456")
-User.create!(id: 3, email: "hello@schooltheyouth.org", password: "123456")
+User.create!(id: 1, email: "bg8s@colgate.edu", password: "123456", is_admin: false)
+User.create!(id: 2, email: "pastapls@yahoo.com", password: "123456", is_admin: false)
+User.create!(id: 3, email: "hello@schooltheyouth.org", password: "123456", is_admin: false)
+User.create!(id: 4, email: "admin@madisoncountygives.com", password: "123456", is_admin: true)
+User.create!(id: 5, email: "test@colgate.edu", password: "123456", is_admin: false)
+User.create!(id: 6, email: "test2@colgate.edu", password: "123456", is_admin: false)
 
 Organization.create!(id: 1, user_id: 1, name: "Gates Foundation", primary_contact: "Bill Gates", address: "1 Microsoft Way", email: "bg8s@colgate.edu", description: "Mosquitos are bugs. We're expert debuggers.", is_approved: true, image: seed_image('gates_foundation.png'))
 Organization.create!(id: 2, user_id: 2, name: "Spaghetti 4 Freddy", primary_contact: "Fred Y.", address: "11 Noodle Lane", email: "pastapls@yahoo.com", description: "Give Freddy the Italian hospitality he doesn't deserve, today", is_approved: true, image: seed_image('spaghetti.jpg'))
 Organization.create!(id: 3, user_id: 3, name: "Youth Education", primary_contact: "Jean Erick", address: "95 Charity Ave.", email: "hello@schooltheyouth.org", description: "Bright minds are bright futures", is_approved: true, image: seed_image('youth_education.jpg'))
+Organization.create!(id: 4, user_id: 5, name: "Test Organization", description: "Test description", is_approved: false, image: seed_image('organization.jpeg'))
+Organization.create!(id: 5, user_id: 6, name: "Test Organization 2", description: "Test description 2", is_approved: false, image: seed_image('organization.jpeg'))
 
 Campaign.create!(id: 1, name: "Malaria Vaccines", organization_id: 1, start_date: Date.new(2017, 11, 13), is_active: true, description: "We are funding a large wave of Malaria vaccines for children in rural Zimbabwe.", goal: 300000, image: seed_image('malaria_vaccine.jpeg'))
 Campaign.create!(id: 2, name: "Ravioli Meal for 1", organization_id: 2, start_date: Date.new(2017, 10, 13), is_active: true, description: "Olive Garden is closed all next week and I think I'm going to starve.", goal: 20, image: seed_image('ravioli.jpeg'))
 Campaign.create!(id: 3, name: "Cure for Polio", organization_id: 1, start_date: Date.new(2017, 10, 13), is_active: false, description: "This is an ambitious undertaking; immunization is not enough. We need to cure polio. Again.", goal: 1000000, image: seed_image('polio.jpg'))
 Campaign.create!(id: 4, name: "Textbooks for 1st Graders", organization_id: 3, start_date: Date.new(2017, 11, 13), is_active: true, description: "SAT Prep for Toddlers and Junior Differential Equations needed for a class of 20!", goal: 1000, image: seed_image('textbooks.jpg'))
+
+CampaignChange.create!(id: 1, campaign_id: nil, organization_id: 1, action: "CREATE", name: "Test Campaign", description: "This will be a test campaign change.", start_date: Date.new(2017, 12, 10), goal: 1000, image: seed_image('lab.jpg'))
+CampaignChange.create!(id: 2, campaign_id: 1, organization_id: 1, action: "UPDATE", description: "We are funding a large wave of Malaria vaccines for children in rural Zimbabwe. The fund-raising campaign will last for one year.")
+CampaignChange.create!(id: 3, campaign_id: 3, organization_id: 1, action: "DELETE")
+CampaignChange.create!(id: 4, campaign_id: 2, organization_id: 2, action: "UPDATE", description: "Olive Garden is closed all next month and I think I'm going to starve.")
+CampaignChange.create!(id: 5, campaign_id: nil, organization_id: 3, action: "CREATE", name: "My Test Campaign", description: "This will be a test campaign change.", start_date: Date.new(2017, 12, 10), goal: 2000, image: seed_image('lab.jpg'))
 
 ShortQuestion.create!(id: 1, question: "What is the central goal of your organization?")
 ShortQuestion.create!(id: 2, question: "How does this organization benefit the people of Madison County?")
